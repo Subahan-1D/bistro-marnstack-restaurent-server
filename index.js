2f2623154dcc9f4b536b1a400fb448eae3a445df
subahan ali <subahanislam523@gmail.com>
2024-08-11 05:30:52 -0600

    delete oparation

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const port = process.env.PORT || 9000 ;
+const port = process.env.PORT || 9000;
 
 //MIDDLEWARE
 app.use(cors());
-app.use(express.json())
-
-
-
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yqmtelq.mongodb.net/?appName=Cluster0`;
 
@@ -29,23 +26,39 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("bistroRestaurantDB").collection("menu");
-    const reviewCollection = client.db("bistroRestaurantDB").collection("reviews");
+    const reviewCollection = client
+      .db("bistroRestaurantDB")
+      .collection("reviews");
     const cartCollection = client.db("bistroRestaurantDB").collection("carts");
 
-    app.get('/menu',async(req,res)=>{
-        const result = await menuCollection.find().toArray();
-        res.send(result)
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
     });
-    app.get('/reviews',async(req,res)=>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result)
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
     });
     // carts collection
-    app.post('/carts', async (req,res)=>{
+
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
-    })
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -58,39 +71,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req,res)=>{
-    res.send('Bistro Restaurant Server Side ')
+app.get("/", (req, res) => {
+  res.send("Bistro Restaurant Server Side ");
 });
 
-app.listen(port,()=>{
-    console.log(`Bistro Restaurant : ${port}`)
+app.listen(port, () => {
+  console.log(`Bistro Restaurant : ${port}`);
 });
