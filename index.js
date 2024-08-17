83bedb8f93c9c4c581c8e3ce687895758d4b6dc1
subahan ali <subahanislam523@gmail.com>
2024-08-17 23:50:36 +0600

    display stats analytics and the admin home

--- index.js
+++ index.js
@@ -203,16 +203,16 @@ async function run() {
       res.send(result);
     });
 
-    // payment intent api all 
+    // payment intent api all
 
     // payment history specie
-    app.get("/payments/:email",verifyToken, async (req, res) => {
-      const query = { email : req.params.email}
-      if(req.params.email !== req.decoded.email){
-        return res.status(403).send({message : 'forbidden access'})
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      const result = await paymentCollection.find(query).toArray()
-      res.send(result)
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
     });
 
     app.post("/create-payment-intent", async (req, res) => {
@@ -245,6 +245,32 @@ async function run() {
       };
       const deleteResult = await cartCollection.deleteMany(query);
       res.send({ paymentResult, deleteResult });
+    });
+
+    // stats analytics
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+      // this is not the best way
+      // const payments = await paymentCollection.find().toArray();
+      // const revenue = payments.reduce((total,payment) => total + payment.price,0)
+      // revenue to pipeline
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+      res.send({ users, menuItems, orders, revenue });
     });
 
     // Send a ping to confirm a successful connection
